010ed7722841c66ab17edb6b3790acce9656b11b
VanQuyet04 <truongquyet137@gmail.com>
2024-07-11 01:40:20 +0700

    ver0  detail screen

---
+++ app/src/main/java/com/example/newsapp/presentation/details/DetailsScreen.kt
@@ -0,0 +1,127 @@
+package com.example.newsapp.presentation.details
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.newsapp.R
+import com.example.newsapp.domain.model.Article
+import com.example.newsapp.domain.model.Source
+import com.example.newsapp.presentation.Dimens
+import com.example.newsapp.presentation.details.components.DetailsTopBar
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun DetailsScreen(
+    article: Article,
+    event: (DetailsEvent) -> Unit,
+    navigateUp: () -> Unit
+) {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        DetailsTopBar(
+            onBrowsingClick = {
+                Intent(Intent.ACTION_VIEW).also {
+                    it.data = Uri.parse(article.url)
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onShareClick = {
+                Intent(Intent.ACTION_SEND).also {
+                    it.putExtra(Intent.EXTRA_TEXT, article.url)
+                    it.type = "text/plain"
+
+                }
+            },
+            onBookmarkClick = {
+                event(DetailsEvent.SaveArticle)
+            },
+            onBackClick = navigateUp
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                end = 8.dp,
+                top = 16.dp,
+            )
+        ) {
+            item {
+                AsyncImage(
+                    model = ImageRequest.Builder(context).data(article.urlToImage).build(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(Dimens.ArticleImageHeight)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(Dimens.MediumPadding1))
+
+                Text(
+                    text = article.title,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = colorResource(id = R.color.text_title),
+                )
+                Text(
+                    text = article.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = colorResource(id = R.color.body),
+                )
+
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DetailsScreenPreview() {
+
+    NewsAppTheme(dynamicColor = false) {
+        DetailsScreen(
+            article = Article(
+                author = "",
+                content = "",
+                description = "",
+                publishedAt = "2 hours",
+                source = Source(id = "", name = "BBC"),
+                title = "Con chó Jack 5 củ",
+                url = "",
+                urlToImage = ""
+            ),
+            event = {}
+        ) {
+
+        }
+    }
+
+}
+
